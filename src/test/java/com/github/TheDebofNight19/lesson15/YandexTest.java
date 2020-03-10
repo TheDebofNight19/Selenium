8fd9eb6cf1dbb27e47274db6283524a4908d671c
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-10 18:53:59 +0300

    Изменено название пакета

--- src/test/java/com/github/TheDebofNight19/lesson15/YandexTest.java
+++ src/test/java/com/github/TheDebofNight19/lesson15/YandexTest.java
@@ -1,4 +1,4 @@
-package com.github.TheDebofNight19.lesson1;
+package com.github.TheDebofNight19.lesson15;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,7 +10,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
 
