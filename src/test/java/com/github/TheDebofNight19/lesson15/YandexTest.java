a84dcfd294f162d70fda607b6221f546183d45ef
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-10 22:04:42 +0300

    Изменено название пакета

--- src/test/java/com/github/TheDebofNight19/lesson15/YandexTest.java
+++ src/test/java/com/github/TheDebofNight19/lesson15/YandexTest.java
@@ -3,37 +3,41 @@ package com.github.TheDebofNight19.lesson15;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
-
 public class YandexTest {
+
+        private static final Logger LOG = LoggerFactory.getLogger(YandexTest.class);
 
         private WebDriver webDriver;
 
         @BeforeSuite
         public void initDriver(){
-            System.setProperty("webdriver.gecko.driver","C:\\Geckodriver\\geckodriver.exe");
-            webDriver = new FirefoxDriver();
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\ЮиЖе\\IdeaProjects\\SeleniumDemo1" +
+                    "\\src\\test\\java\\resources\\suites\\chromedriver.exe");
+            webDriver = new ChromeDriver();
         }
-
 
         @Test
         public void testPage(){
             webDriver.get("https://yandex.ru");
-            WebElement input = webDriver.findElement(By.name("text"));
-            input.sendKeys("Руддщ цщкдв!"); //вбили Hello world
-            input.sendKeys(Keys.ENTER); //нажали Enter
-            webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-            WebElement spanElement = webDriver.findElement(By.className("misspell__message"));
-            String s = spanElement.getText();
-            Assert.assertEquals(s, "Исправлена раскладка клавиатуры с «Руддщ цщкдв!»");
+            WebElement input1 = webDriver.findElement(By.name("text"));
+            input1.sendKeys("Руддщ цщкдв!"); //вбили Hello world
+            input1.sendKeys(Keys.ENTER); //нажали Enter
+            String s = webDriver.getTitle();
+            WebElement input2 = webDriver.findElement(By.name("text"));
+            String t = input2.getAttribute("value");
+            Assert.assertTrue(s.contains("Hello world! — Яндекс"));
+            Assert.assertTrue(t.contains("Hello world!"));
+            LOG.info(s);
+            LOG.info(t);
 
         }
 
