31031cebc0e84358c9356e4e2edf17cae02ca7d6
EZvyagintsev <zvyagintsev.e@gmail.com>
2020-03-10 18:51:42 +0300

    Test modified

--- src/test/java/com/github/TheDebofNight19/lesson1/YandexTest.java
+++ src/test/java/com/github/TheDebofNight19/lesson1/YandexTest.java
@@ -1,16 +1,45 @@
 package com.github.TheDebofNight19.lesson1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
 
 
 public class YandexTest {
-        @Test
-        public void testYandex(){
-            System.setProperty("webdriver.gecko.driver","C:\\Geckodriver\\geckodriver.exe");
-            WebDriver webDriver = new FirefoxDriver();
-            webDriver.get("https://yandex.ru/ncr");
 
+        private WebDriver webDriver;
+
+        @BeforeMethod
+        public void initDriver(){
+            System.setProperty("webdriver.gecko.driver","C:\\Geckodriver\\geckodriver.exe");
+            webDriver = new FirefoxDriver();
+        }
+
+
+        @Test
+        public void testPage(){
+            webDriver.get("https://yandex.ru");
+            WebElement input = webDriver.findElement(By.name("text"));
+            input.sendKeys("Руддщ цщкдв!"); //вбили Hello world
+            input.sendKeys(Keys.ENTER); //нажали Enter
+            webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            WebElement spanElement = webDriver.findElement(By.className("misspell__message"));
+            String s = spanElement.getText();
+            Assert.assertEquals(s, "Исправлена раскладка клавиатуры с «Руддщ цщкдв!»");
+
+        }
+
+        @AfterMethod
+        public void closeDriver(){
+            webDriver.quit();
         }
 }
